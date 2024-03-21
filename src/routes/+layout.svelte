84375063b9caf7541620548b544c0f0edfdab9d3
Lera <valeriya_pimenova@list.ru>
2024-03-21 20:19:27 +0900

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,11 @@
     <div class="nav-container">
     <a href="/" class="nav-logo" title="Back to Homepage">Лера Пименова </a>
      <div class="nav-links">
-        {#each nav as link}
+        {#if link.isShow}
 
-        <a href={link.href} class="link">{link.title}</a>
-        
-        {/each}
+<a href={link.href} class="link">{link.title}</a>
+
+{/if}
     </div>
     </div>
     </nav>
@@ -19,25 +19,30 @@
         const nav = [
             {
                 title: 'Обо мне',
-                href:'/about'
+                href:'/about',
+                isShow: true,
             },
             {
                 title: 'Блог',
-                href:'/blog'
+                href:'/blog',
+                isShow: true,
             },
             {
                 title: 'Портфолио',
-                href:'/project'
+                href:'/project',
+                isShow: true,
             },
             {
                 title: 'Контакты',
-                href:'/contact'
+                href:'/contact',
+                isShow: true,
             },
             {
                 title: 'temp',
-                href:'/temp'
+                href:'/temp',
+                isShow: false,
             }
-        ]
+        ];
     </script>
 
     <style>
